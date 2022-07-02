f3f62e7e27f49b97be45f01c9adf0a9985988062
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2022-07-02 16:35:33 -0500

    Estructura

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const certificados = document.getElementById('cert');
+
+
+const centerhtml=document.createElement('center');
+centerhtml.classList.add('center');
+
+certificados.appendChild(centerhtml);
