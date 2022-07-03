2b363ca28d890f890f6a8639aa30a5517c16d1bd
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-07-02 19:25:23 -0500

    ADD: solucion de generacion automatica de certificados

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,49 @@
-const certificados = document.getElementById('cert');
+const arrayCertificados = [
+    {
+        imgUrl: "./assets/img/cert1.PNG",
+        title: "CONECTA EMPLEO2",
+        name: "Programacion con JavaScript",
+        date: "13 de mayo del 2022",
+        link: "https://drive.google.com/file/d/1g4zLf3ydXMA91qoJ7JtoytB8EZYF2c7F/view?usp=sharing"
+    },
+    {
+        imgUrl: "./assets/img/cert1.PNG",
+        title: "Marioa",
+        name: "Programacion con JavaScript",
+        date: "13 de mayo del 2022",
+        link: "https://drive.google.com/file/d/1g4zLf3ydXMA91qoJ7JtoytB8EZYF2c7F/view?usp=sharing"
+    },
+    {
+        imgUrl: "./assets/img/cert1.PNG",
+        title: "CONECTA EMPLEO",
+        name: "Programacion con JavaScript",
+        date: "13 de mayo del 2022",
+        link: "https://drive.google.com/file/d/1g4zLf3ydXMA91qoJ7JtoytB8EZYF2c7F/view?usp=sharing"
+    }
+];
+const listCertificados = document.getElementById('lista-certificados');
 
-
-const centerhtml=document.createElement('center');
-centerhtml.classList.add('center');
-
-certificados.appendChild(centerhtml);
+const createCerticatesHtml = () => {
+    let templateHTML = "";
+    arrayCertificados.forEach(e => {
+        templateHTML += `
+        <center class="col">
+                    <div id="width" class="card col-6 m-2"">
+                        <img src="${e.imgUrl}" class="card-img-top img_cert" alt="...">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" style="background-color: #12116d; color:white"><strong>${e.title}</strong></li>
+                            <li class="list-group-item">${e.name}</li>
+                            <li class="list-group-item">${e.date}</li>
+                        </ul>
+                        <div class="card-body">
+                            <a target="_blank"
+                                href="${e.link}"
+                                class="card-link">Certificado en PDF</a>
+                        </div>
+                    </div>
+        </center>
+        `;
+    });
+    listCertificados.innerHTML = templateHTML;
+}
+createCerticatesHtml();
